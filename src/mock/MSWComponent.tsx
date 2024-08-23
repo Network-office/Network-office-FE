4cd058a8780a817b8d6ac8c88af84becd2299e7f
JangGwon <tlsakch510@naver.com>
2024-08-23 13:29:22 +0900

    feat : .env NEXT_PUBLIC_MOCKING에 따른 모킹 onoff기능 추가

--- src/mock/MSWComponent.tsx
+++ src/mock/MSWComponent.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 export const MSWComponent = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      {
+      if (process.env.NEXT_PUBLIC_MOCKING === "enabled") {
         const init = async () => {
           const { worker } = await import("@/mock/browser")
           worker.start()
