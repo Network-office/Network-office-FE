213e1ecd9f4a682034f3f4b4f6a04b2b24f430da
JangGwon <tlsakch510@naver.com>
2024-08-23 19:38:22 +0900

    refactor : 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-"use client"
-
 export default function Home() {
-  const adf = async () => {
-    const abcd = await fetch("http://localhost:8080/test", {
-      cache: "no-store"
-    })
-    const result = await abcd.json()
-    console.log(result)
-  }
-  adf()
   return <main></main>
 }
