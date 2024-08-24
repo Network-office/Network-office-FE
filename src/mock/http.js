944afae20c4342b2ad0e9a9f5cddf21f55d18924
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2024-08-24 14:51:52 +0900

    Settings : MSW + express를 이용한 Mocking 서버 구축 & Faker.js 라이브러리 적용 (#5)
    * settings : MSW(v-2.3.5) 라이브러리 추가
    
    * feat : MSW 브라우저 환경 추가
    
    * feat : .env NEXT_PUBLIC_MOCKING에 따른 모킹 onoff기능 추가
    
    * settings : express+msw 서버 설치완료
    
    * settings : strictMode : false 설정
    
    * settings : fakerjs 설치
    
    * refactor : 테스트 코드 제거

---
+++ src/mock/http.js
@@ -0,0 +1,50 @@
+import express from "express"
+import { createMiddleware } from "@mswjs/http-middleware"
+import cors from "cors"
+import { handlers } from "./handler"
+
+const app = express()
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200,
+    credentials: true
+  })
+)
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`)
+  next()
+})
+
+const middleware = createMiddleware(...handlers)
+
+app.use((req, res, next) => {
+  let oldWrite = res.write
+  let oldEnd = res.end
+  let chunks = []
+
+  res.write = (...args) => {
+    chunks.push(Buffer.from(args[0]))
+    return oldWrite.apply(res, args)
+  }
+
+  res.end = (...args) => {
+    if (args[0]) {
+      chunks.push(Buffer.from(args[0]))
+    }
+
+    const body = JSON.parse(Buffer.concat(chunks))
+
+    console.log(`Response: ${res.statusCode} ${req.url} ${body}`)
+    return oldEnd.apply(res, args)
+  }
+
+  middleware(req, res, next)
+})
+
+app.listen(8080, () => {
+  console.log("CREATE MOCK SERVER")
+})
