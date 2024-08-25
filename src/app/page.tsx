c9fc198bdf021a51a5dae2bc64784db8c5a866e7
JangGwon <tlsakch510@naver.com>
2024-08-25 16:22:28 +0900

    feat : pushstep : 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+"use client"
+
+import { useFunnel } from "@/_common/_hooks/useFunnel"
+
 export default function Home() {
-  return <main></main>
+  const [Funnel, setStep, step] = useFunnel(["funnel1", "funnel2", "funnel3"])
+
+  return (
+    <main>
+      <Funnel>
+        <Funnel.Step name="funnel1">
+          <p>지금은 {step} Funnel</p>
+          <button onClick={() => setStep("funnel2")}>다음 퍼널로</button>
+        </Funnel.Step>
+        <Funnel.Step name="funnel2">
+          <p>지금은 {step} Funnel</p>
+          <button onClick={() => setStep("funnel3")}>다음 퍼널로</button>
+        </Funnel.Step>
+        <Funnel.Step name="funnel3">
+          <p>지금은 {step} Funnel</p>
+          <button onClick={() => setStep("funnel3")}>다음 퍼널로</button>
+        </Funnel.Step>
+      </Funnel>
+    </main>
+  )
 }
