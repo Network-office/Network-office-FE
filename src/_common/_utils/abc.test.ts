992de0c0c62250717d510d57d7ecb86d8d0cf605
JangGwon <tlsakch510@naver.com>
2024-08-27 17:27:55 +0900

    test : 임시 테스트 적용

---
+++ src/_common/_utils/abc.test.ts
@@ -0,0 +1,23 @@
+// abc.test.ts
+import abc from "./abc"
+import { describe, test, expect } from "@jest/globals"
+
+describe("abc function", () => {
+  test("should correctly add two positive numbers", () => {
+    expect(abc(1, 2)).toBe(3)
+  })
+
+  test("should correctly add two negative numbers", () => {
+    expect(abc(-1, -2)).toBe(-3)
+  })
+
+  test("should correctly add a positive number and a negative number", () => {
+    expect(abc(1, -2)).toBe(-2)
+  })
+
+  test("should correctly add zero", () => {
+    expect(abc(0, 0)).toBe(0)
+    expect(abc(0, 5)).toBe(5)
+    expect(abc(5, 0)).toBe(5)
+  })
+})
