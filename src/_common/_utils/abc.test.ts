ff5486fd7db55fedd6622eed22c81f4b4e98b9f1
JangGwon <tlsakch510@naver.com>
2024-08-29 16:53:55 +0900

    test : 테스트 추가

--- src/_common/_utils/abc.test.ts
+++ src/_common/_utils/abc.test.ts
@@ -14,7 +14,13 @@ describe("abc function", () => {
   test("should correctly add a positive number and a negative number", () => {
     expect(abc(1, -2)).toBe(-2)
   })
+  test("should correctly add a positive number and a negative number", () => {
+    expect(abc(1, -2)).toBe(0)
+  })
 
+  test("should correctly add a positive number and a negative number", () => {
+    expect(abc(1, -2)).toBe(1)
+  })
   test("should correctly add zero", () => {
     expect(abc(0, 0)).toBe(0)
     expect(abc(0, 5)).toBe(5)
