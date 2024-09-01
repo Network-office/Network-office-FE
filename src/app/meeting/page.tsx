d3566d7a5f91d2f40eea426273fafb8ea6f528d6
JangGwon <tlsakch510@naver.com>
2024-09-02 02:52:26 +0900

    design : 메인홈에 검색바 배치 및 디자인

--- src/app/meeting/page.tsx
+++ src/app/meeting/page.tsx
@@ -2,24 +2,33 @@
 
 import useNaverMap from "@/_common/_hooks/useNaverMap"
 import Topbar from "@/_common/_components/Topbar"
+import SearchBar from "./_components/SearchBar"
 
-const mockData = { lat: 37, lng: 127 }
+const mockData = [
+  { lat: 37, lng: 127 },
+  { lat: 37.001, lng: 127.002 },
+  { lat: 37.005, lng: 127.003 }
+]
 
 const Meeting = () => {
-  const { NaverMapComponent, setMarkers } = useNaverMap({ lat: 37, lng: 127 }, [
+  const { NaverMapComponent, setMarkers } = useNaverMap(
+    { lat: 37, lng: 127 },
     mockData
-  ])
+  )
   return (
     <div className="relative text-black">
-      <Topbar
-        className="absolute w-screen z-10 h-[14px] border-0"
-        rightContent={
-          <div className="flex gap-2">
-            <Topbar.ProfileLink />
-            <Topbar.AlarmLink />
-          </div>
-        }
-      />
+      <div className="absolute z-10">
+        <Topbar
+          className=" w-screen h-[14px] border-0"
+          rightContent={
+            <div className="flex gap-2">
+              <Topbar.ProfileLink />
+              <Topbar.AlarmLink />
+            </div>
+          }
+        />
+        <SearchBar />
+      </div>
       <NaverMapComponent className="absolute top-0 z-0" />
     </div>
   )
