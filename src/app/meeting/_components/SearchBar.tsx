a2ef1869cdf3933ff05ee8685b7392a454cb1481
JangGwon <tlsakch510@naver.com>
2024-09-02 02:54:25 +0900

    design : 검색바 크기 증가 w-80% -> w-85% & h-36px -> h-40px

--- src/app/meeting/_components/SearchBar.tsx
+++ src/app/meeting/_components/SearchBar.tsx
@@ -2,7 +2,7 @@ import { Search, AlignJustifyIcon } from "lucide-react"
 
 const SearchBar = () => {
   return (
-    <div className="w-[80%] h-[36px] mt-4 mx-auto rounded-lg  border-black  bg-white drop-shadow-xl flex gap-2 justify-center items-center">
+    <div className="w-[85%] h-[40px] mt-3 mx-auto rounded-lg  border-black  bg-white drop-shadow-xl flex gap-2 justify-center items-center">
       <AlignJustifyIcon size={24} />
       <input className="w-[70%] border-[1px] rounded-sm px-2" />
       <Search
