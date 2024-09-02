8010b68f7de7722588940cccb18e2df77df4df68
JangGwon <tlsakch510@naver.com>
2024-09-02 22:30:04 +0900

    test : 테스트 코드 추가

--- src/_common/_utils/types.ts
+++
@@ -1,9 +0,0 @@
-export interface getUserGeolocationResponseType {
-  ok: boolean
-  position?: { lat: number; lng: number }
-  errorMessage?: string
-}
-
-export interface LocationResponseType {
-  coords: { latitude: number; longitude: number }
-}
