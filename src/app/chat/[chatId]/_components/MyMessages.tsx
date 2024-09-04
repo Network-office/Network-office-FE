ace2bc3a66327d79fa83613a1b76ad8d56211fb4
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-09-04 15:37:32 +0900

    feat: MyMessages 에도 MessageHeader 컴포넌트 사용

--- src/app/chat/[chatId]/_components/MyMessages.tsx
+++ src/app/chat/[chatId]/_components/MyMessages.tsx
@@ -1,11 +1,15 @@
-import { Message } from "@/app/chat/[chatId]/_components/Message"
+import { Message, MessageHeader } from "@/app/chat/[chatId]/_components/Message"
 import { MyMessageProps } from "@/app/chat/[chatId]/_components/types"
-import { Badge } from "@/components/ui/badge"
 
-const MyMessages = ({ messages, timestamp, role }: MyMessageProps) => {
+const MyMessages = ({ messages, userInfo, role }: MyMessageProps) => {
   return (
     <div className="flex flex-col gap-4 w-full pb-4">
-      {role === "admin" && <Badge>Admin</Badge>}
+      <MessageHeader
+        username={userInfo.username}
+        avatarSrc={userInfo.avatarSrc}
+        badgeText={role === "admin" ? "Admin" : ""}
+        align="right"
+      />
       {messages.map((message, i) => (
         <div
           key={i}
@@ -13,7 +17,9 @@ const MyMessages = ({ messages, timestamp, role }: MyMessageProps) => {
           <Message
             align="right"
             text={message.text}
-            timestamp={messages.length - 1 === i ? timestamp : undefined}
+            timestamp={
+              messages.length - 1 === i ? messages[i].timestamp : undefined
+            }
           />
         </div>
       ))}
