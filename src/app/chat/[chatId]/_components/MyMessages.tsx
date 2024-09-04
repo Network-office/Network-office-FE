793985e2dc06fbc60d0a373cc31d580e4a178af3
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-09-04 12:48:10 +0900

    feat: MyMessage 컴포넌트 테스팅

--- src/app/chat/[chatId]/_components/MyMessages.tsx
+++ src/app/chat/[chatId]/_components/MyMessages.tsx
@@ -1,9 +1,11 @@
 import { Message } from "@/app/chat/[chatId]/_components/Message"
 import { MyMessageProps } from "@/app/chat/[chatId]/_components/types"
+import { Badge } from "@/components/ui/badge"
 
-const MyMessages = ({ messages, timestamp }: MyMessageProps) => {
+const MyMessages = ({ messages, timestamp, role }: MyMessageProps) => {
   return (
     <div className="flex flex-col gap-4 w-full pb-4">
+      {role === "admin" && <Badge>Admin</Badge>}
       {messages.map((message, i) => (
         <div
           key={i}
---
+++ src/app/chat/[chatId]/_components/_tests/Messages.spec.tsx
@@ -0,0 +1,118 @@
+import MyMessages from "@/app/chat/[chatId]/_components/MyMessages"
+import { Message } from "@/mock/types"
+import { faker } from "@faker-js/faker"
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  jest,
+  test
+} from "@jest/globals"
+import { render, screen } from "@testing-library/react"
+
+const generateMessage = (): Message => ({
+  id: faker.string.uuid(),
+  text: faker.lorem.sentence(),
+  role: faker.helpers.arrayElement<"user" | "admin">(["user", "admin"]),
+  createdAt: faker.date.recent().toISOString(),
+  me: faker.datatype.boolean()
+})
+
+let messages: Message[]
+let adminMessages: Message[]
+let userMessages: Message[]
+let myMessages: Message[]
+
+describe("MyMessages", () => {
+  beforeEach(() => {
+    messages = Array.from({ length: 5 }, generateMessage)
+    adminMessages = messages.filter((message) => message.role === "admin")
+    userMessages = messages.filter((message) => message.role === "user")
+    myMessages = messages.filter((message) => message.me)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test("모든 메시지를 출력한다", () => {
+    render(
+      <MyMessages
+        messages={myMessages}
+        role={myMessages[0].role}
+      />
+    )
+    myMessages.forEach((message) => {
+      expect(document.body).toHaveTextContent(message.text)
+    })
+  })
+
+  test("마지막 메시지에만 타임스탬프를 출력한다", () => {
+    const timestamp = faker.date.recent().toISOString()
+
+    const { getByText } = render(
+      <MyMessages
+        messages={myMessages}
+        role={myMessages[0].role}
+        timestamp={new Date(timestamp).getTime()}
+      />
+    )
+
+    // 타임스탬프가 한번만 출력되어야 한다
+    const renderedTimestamp = getByText(
+      new Date(timestamp).toLocaleTimeString()
+    )
+
+    expect(renderedTimestamp).toBeInTheDocument()
+
+    // 마지막에 타임스탬프가 출력되어야 한다
+    const lastMessage = myMessages[myMessages.length - 1]
+    const lastMessageComponent = getByText(lastMessage.text)
+
+    expect(lastMessageComponent.nextSibling).toBe(renderedTimestamp)
+  })
+
+  test("삼각뿔이 오른쪽에 위치해야 한다", () => {
+    const { getByText } = render(
+      <MyMessages
+        messages={myMessages}
+        role={myMessages[0].role}
+      />
+    )
+
+    const rightTriangle =
+      "after:border-l-8 after:border-y-transparent after:border-y-8 after:border-r-0"
+
+    myMessages.forEach((message) => {
+      const messageComponent = getByText(message.text)
+      expect(messageComponent).toHaveClass(rightTriangle)
+    })
+  })
+
+  test("관리자라면 뱃지를 처음 한번 출력한다", () => {
+    const { getByText } = render(
+      <MyMessages
+        messages={adminMessages}
+        role="admin"
+      />
+    )
+
+    const badge = getByText("Admin")
+
+    expect(badge).toBeInTheDocument()
+  })
+
+  test("관리자가 아니라면 뱃지를 출력하지 않는다", () => {
+    const { queryByText } = render(
+      <MyMessages
+        messages={userMessages}
+        role="user"
+      />
+    )
+
+    const badge = queryByText("Admin")
+
+    expect(badge).toBeNull()
+  })
+})
