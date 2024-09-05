525fe5d2ed1c4ec20082647497db8dab5a31062a
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2024-09-05 17:42:48 +0900

    settings : Msw-nodejs 세팅 및 jest에 msw 연결 완료 (#39)

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,10 @@ import "@testing-library/jest-dom"
 import "@testing-library/react"
 import "@testing-library/jest-dom/jest-globals"
 import "@testing-library/react-hooks"
+
+import { beforeAll, afterEach, afterAll } from "@jest/globals"
+import { server } from "@/mock/node"
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
