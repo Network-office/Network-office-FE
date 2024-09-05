7c315be01b354422babe2da9d9331487bcc7c47c
kimhyojung <706shin1728@naver.com>
2024-09-06 01:15:29 +0900

    Settings: QueryProvider, return-fetch 설정 (#40)
    * setting : 네이버 지도 APi 타입 스크립트 패키지 추가
    
    * feat : NaverMap 출력하는 컴포넌트 생성
    
    * feat : NaverApi geocoder 연동 및 naverAPi 방식 변경
    
    * feat : 지명 검색 기능 추가
    
    * refactor : NaverMap컴포넌트 useNaverMap 훅으로 변경
    
    * refactor : useNaverMap훅 매개변수 값 변경
    
    * refactor : useNvaerMap 훅 매개 변수 변경
    
    * test : 테스트용 console.log 전부 제거
    
    * chore: add fetchUtil
    
    * feat: queryprovider 설정
    
    * refactor: 네이버 맵 훅 변경
    
    * refactor : 테스트 파일 제거
    
    * refactor : 불필요한 파일 제거
    
    ---------
    
    Co-authored-by: JangGwon <tlsakch510@naver.com>
    Co-authored-by: JangGwon <40955023+JuJangGwon@users.noreply.github.com>

---
+++ src/lib/http.ts
@@ -0,0 +1,53 @@
+import returnFetch, { FetchArgs } from "return-fetch"
+
+const createHTTP = () => {
+  return async <T>(
+    input: URL | RequestInfo,
+    init?: RequestInit
+  ): Promise<{ data: T }> => {
+    return returnFetch({
+      //로컬 or 서버 환경 구분하는 유틸 필요
+      baseUrl: "",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      interceptors: {
+        request: async (config: FetchArgs) => {
+          //요청에 헤더 있을 시 병합
+          config[1] = {
+            ...config[1],
+            ...init
+          }
+          return config
+        },
+
+        response: async (response: Response) => {
+          const responseBody = await response.json()
+
+          return new Response(JSON.stringify(responseBody))
+        }
+      }
+    })(input, init).then((response) =>
+      response.json().then((data: T) => ({
+        data
+      }))
+    )
+  }
+}
+
+// 클라이언트 사이드용 함수
+export const clientHTTP = createHTTP()
+
+// 환경에 따라 적절한 함수를 선택하는 래퍼 함수
+export const http = <T>(
+  input: URL | RequestInfo,
+  init?: RequestInit
+): Promise<{ data: T }> => {
+  if (typeof window !== "undefined") {
+    return clientHTTP<T>(input, init)
+  }
+
+  //Todo-서버 사이드 용 fetch함수
+  return clientHTTP<T>(input, init)
+}
