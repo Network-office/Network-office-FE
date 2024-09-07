550474afedeedd15e006cd755de3c4b0d018653a
JangGwon <tlsakch510@naver.com>
2024-09-08 02:22:07 +0900

    design : 모임 카테고리 선정 페이지 디자인

--- src/app/meeting/create/_components/_funnels/MeetingCategory.tsx
+++ src/app/meeting/create/_components/_funnels/MeetingCategory.tsx
@@ -2,21 +2,25 @@ interface MeetingCategoriesProps {
   onNextStep: () => void
 }
 
-const category = ["스포츠", "술", "맛집"]
+const category = ["스포츠", "술", "맛집", "봉사", "영화/연극", "기타"]
 
 const MeetingCategory = ({ onNextStep }: MeetingCategoriesProps) => {
   return (
     <div>
-      <h1>모임생성</h1>
-      <div className="grid grid-cols-2">
+      <h1 className="mx-4 text-[24px] font-bold text-center mt-8">
+        모임 카테고리를 선택해주세요.
+      </h1>
+      <div className="grid grid-cols-2 w-[300px] mx-auto mt-6">
         {category.map((item, index) => (
-          <p key={index}>item</p>
+          <button className="w-[140px] h-[140px] mb-2 rounded-md bg-blue-400">
+            {item}
+          </button>
         ))}
       </div>
       <button
-        className="w-[70%] h-[30px] mx-auto bg-blue-300 rounded-md "
+        className="w-[70%] h-[40px] mt-4 mx-auto bg-blue-300 rounded-md flex justify-center text-center py-auto"
         onClick={onNextStep}>
-        다음으로
+        <span className="my-auto text-white font-semibold">다음으로</span>
       </button>
     </div>
   )
