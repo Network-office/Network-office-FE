a4d5d0e3c24de8b96c0945491acfaa5d581b2293
JangGwon <tlsakch510@naver.com>
2024-09-10 02:38:16 +0900

    feat : 모임 동네 선택 퍼널 구현

--- src/app/meeting/create/_components/_funnels/MeetingPlace.tsx
+++ src/app/meeting/create/_components/_funnels/MeetingPlace.tsx
@@ -1,23 +1,54 @@
-import Input from "@/_common/_components/Input"
-import { Search } from "lucide-react"
+"use client"
 
+import { useState } from "react"
+import { Search } from "lucide-react"
+import searchPlace from "@/_common/_utils/searchPlace"
+import PlaceTypes from "@/_common/_utils/searchPlace/type"
 interface MeetingTitleInputProps {
   onNextStep: () => void
 }
 
 const MeetingPlace = ({ onNextStep }: MeetingTitleInputProps) => {
+  const [inputKeyword, setInputKeyWord] = useState("")
+  const [searchResult, setSearchResult] = useState<PlaceTypes[]>()
+
+  const searchBarhandle = async () => {
+    const result = await searchPlace(inputKeyword)
+    setSearchResult(result)
+  }
+
+  const onClickPlaceButton = (selectedPlace: PlaceTypes) => {
+    console.log(selectedPlace)
+    setInputKeyWord("")
+    setSearchResult([])
+    onNextStep()
+  }
   return (
     <div className="w-screen">
       <h1 className="mx-4 text-[28px] font-bold text-center mt-12">
-        모임 장소는 어디가 좋을까요
+        모임 장소는 어디가 좋을까요?
       </h1>
+      <h2 className="text-center text-slate-400 text-sm"></h2>
       <div className="w-[300px] h-[30px] border-b-2 flex mx-auto mt-14">
         <input
-          className="w-[280px] h-[30px] border-b-2"
-          mx-auto
+          onChange={(event) => setInputKeyWord(event.target.value)}
+          onKeyUp={searchBarhandle}
+          placeholder="동네 이름을 적어주세요!"
+          className="w-[280px] h-[30px] border-b-2 focus:outline-none pl-3 text-2xl pb-2"
         />
-        <Search />
+        <Search className="mb-2 mr-1" />
       </div>
+      <ul className="w-[75%] h-[80%] mx-auto">
+        {searchResult?.map((resultItem, index) => (
+          <li
+            className="w-full h-[50px] border-[0.8px] border-slate-300 flex items-center pl-3"
+            key={index}>
+            <button onClick={() => onClickPlaceButton(resultItem)}>
+              {resultItem.jibunAddress}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- src/app/meeting/create/_components/_funnels/MeetingTitleInput.tsx
+++ src/app/meeting/create/_components/_funnels/MeetingTitleInput.tsx
@@ -13,10 +13,7 @@ const MeetingTitleInput = ({ onNextStep }: MeetingTitleInputProps) => {
       <h3 className="text-gray-400 text-[12px] text-center">
         모임 제목은 8자리에서 최대 20자리 미만으로 작성해주세요{" "}
       </h3>
-      <Input
-        className="w-[70%] h-[50px] mx-auto mt-20"
-        mx-auto
-      />
+      <Input className="w-[70%] h-[50px] mx-auto mt-20" />
       <button
         className="w-[70%] h-[40px] mt-8 mx-auto bg-blue-300 rounded-md flex justify-center text-center py-auto"
         onClick={onNextStep}>
