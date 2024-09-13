1993f906052d912e5c0768bd351e64ca0c2d5d55
JangGwon <tlsakch510@naver.com>
2024-09-13 19:06:34 +0900

    test : 모임 상세 정보 조회 모킹 api 구현

--- src/mock/api/meetingApi.js
+++ src/mock/api/meetingApi.js
@@ -6,6 +6,24 @@ const handlers = [
     return new HttpResponse(JSON.stringify({ content: [...meetingData] }), {
       status: 200
     })
+  }),
+  http.get(`http://localhost:8080/api/meeting/:meetingId`, ({ params }) => {
+    const meetingId = Number(params.auctionId)
+    const meetingDetail = meetingData.find(
+      (meetingItem) => meetingItem.id === meetingId
+    )
+    if (meetingDetail) {
+      return new HttpResponse(JSON.stringify({ data: meetingDetail }), {
+        status: 200
+      })
+    } else {
+      return new HttpResponse(
+        JSON.stringify({ error: "해당 모임 정보가 존재하지 않습니다." }),
+        {
+          status: 400
+        }
+      )
+    }
   })
 ]
 
