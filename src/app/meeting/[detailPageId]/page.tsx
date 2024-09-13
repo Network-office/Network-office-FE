073987e32019ccd87a534bfff18497ac1ddf96d7
JangGwon <tlsakch510@naver.com>
2024-09-13 18:27:35 +0900

    feat : 모임상세페이지에서 모임 위치 지도 보여주기 기능 구현

--- src/app/meeting/[detailPageId]/page.tsx
+++ src/app/meeting/[detailPageId]/page.tsx
@@ -1,11 +1,14 @@
+"use client"
+
 import MeetingDetailHeader from "./_components/MeetingDetailHeader"
 import MeetingContentSection from "./_components/MeetingContentSection"
+import MeetingMapSection from "./_components/MeetingMapSection"
 
 const meetingDetail: MeetingDetailTypes = {
   id: 1,
   author: "John Doe",
   title: "테스트 모임",
-  place: "서울 강남구 어딘가",
+  place: "서울 강남구 주노동 준오아파트 103동 ",
   date: "2024-09-20",
   startTime: 10,
   endTime: 12,
@@ -14,15 +17,22 @@ const meetingDetail: MeetingDetailTypes = {
   nowPeople: 5,
   fee: 5000,
   detail: "이 모임은 함께 운동을 즐기기 위한 모임입니다.",
-  x: 127.027619,
-  y: 37.497942
+  y: 127.027619,
+  x: 37.497942
 }
 
 const MeetingDetailPage = () => {
   return (
-    <div>
+    <div className="w-screen h-screen">
       <MeetingDetailHeader title={meetingDetail.title} />
       <MeetingContentSection meetingDetail={meetingDetail} />
+      <hr />
+      <MeetingMapSection
+        lat={meetingDetail.x}
+        lng={meetingDetail.y}
+        meetingId={meetingDetail.id}
+        place={meetingDetail.place}
+      />
     </div>
   )
 }
--- src/app/meeting/types.ts
+++ src/app/meeting/types.ts
@@ -2,13 +2,13 @@ export interface MeetingInformTypes {
   id: number
   lat: number
   lng: number
-  title: string
-  place: string
-  fare: string
-  totalPeople: number
-  vacancy: number
-  startTime: string
-  endTime: string
+  title?: string
+  place?: string
+  fare?: string
+  totalPeople?: number
+  vacancy?: number
+  startTime?: string
+  endTime?: string
 }
 
 export interface MeetingListResponse {
