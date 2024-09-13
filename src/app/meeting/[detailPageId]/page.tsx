c5a905ff5e73c7559e9b6128b1f43f608dd146c9
JangGwon <tlsakch510@naver.com>
2024-09-13 18:34:24 +0900

    design : 모임 상세 페이지 하단 버튼바 배치

--- src/app/meeting/[detailPageId]/page.tsx
+++ src/app/meeting/[detailPageId]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { HeartOff } from "lucide-react"
 import MeetingDetailHeader from "./_components/MeetingDetailHeader"
 import MeetingContentSection from "./_components/MeetingContentSection"
 import MeetingMapSection from "./_components/MeetingMapSection"
@@ -33,6 +34,14 @@ const MeetingDetailPage = () => {
         meetingId={meetingDetail.id}
         place={meetingDetail.place}
       />
+      <div className="flex justify-between mx-6 mt-[20px]">
+        <button>
+          <HeartOff />
+        </button>
+        <button className="w-[150px] h-[40px] rounded-md bg-blue-300 text-white">
+          참가신청
+        </button>
+      </div>
     </div>
   )
 }
