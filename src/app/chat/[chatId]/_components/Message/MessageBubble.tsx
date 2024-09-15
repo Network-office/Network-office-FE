4bcc659c1a81576339e8e3ce8dfd989b8dc8a7a3
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-09-15 13:13:27 +0900

    refactor: class변수들을 컴포넌트 외부로 이동

--- src/app/chat/[chatId]/_components/Message/MessageBubble.tsx
+++ src/app/chat/[chatId]/_components/Message/MessageBubble.tsx
@@ -3,22 +3,15 @@ export interface MessageBubbleProps {
   align: "left" | "right"
   text: string
 }
-export const MessageBubble = ({ align, text }: MessageBubbleProps) => {
-  const baseClasses = " relative bg-slate-300 w-fit p-2 rounded-xl text-xs"
-  const triangleClasses = {
-    left: "ml-4 after:top-1/2 after:-left-1.5 after:-translate-y-1/2 after:border-r-slate-300 after:border-r-8 after:border-y-transparent after:border-y-8 after:border-l-0",
-    right:
-      "mr-4 after:top-1/2 after:-right-1.5 after:-translate-y-1/2 after:border-l-slate-300 after:border-l-8 after:border-y-transparent after:border-y-8 after:border-r-0"
-  }
 
-  return (
-    <p
-      className={cn(
-        baseClasses,
-        triangleClasses[align],
-        " after:absolute after:block after:w-0 after:h-0"
-      )}>
-      {text}
-    </p>
-  )
+const baseClasses =
+  " relative bg-slate-300 w-fit p-2 rounded-xl text-xs after:absolute after:block after:w-0 after:h-0"
+const triangleClasses = {
+  left: "ml-4 after:top-1/2 after:-left-1.5 after:-translate-y-1/2 after:border-r-slate-300 after:border-r-8 after:border-y-transparent after:border-y-8 after:border-l-0",
+  right:
+    "mr-4 after:top-1/2 after:-right-1.5 after:-translate-y-1/2 after:border-l-slate-300 after:border-l-8 after:border-y-transparent after:border-y-8 after:border-r-0"
+}
+
+export const MessageBubble = ({ align, text }: MessageBubbleProps) => {
+  return <p className={cn(baseClasses, triangleClasses[align])}>{text}</p>
 }
