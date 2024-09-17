60cb97ad1e9822e2271e79440f012b7ed7bad841
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-09-17 13:24:05 +0900

    refactor: message 관련 타입 수정

--- src/app/chat/[chatId]/_components/types.ts
+++ src/app/chat/[chatId]/_components/types.ts
@@ -1,10 +1,26 @@
 export interface Message {
   id: string
   text: string
-  timestamp?: number
+  timestamp: number
 }
 
 export interface UserInfo {
+  id: string
   username: string
   avatarSrc: string
+}
+export interface SocketMessageRequest {
+  text: string
+}
+
+export interface SocketMessageResponse {
+  id: string
+  message: Omit<Message, "id">
+  userInfo: {
+    id: string
+    username: string
+    avatarSrc: string
+    me: boolean
+    role: "user" | "admin"
+  }
 }
