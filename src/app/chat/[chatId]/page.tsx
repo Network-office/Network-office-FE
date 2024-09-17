1ceab6edbfbd35b82024e813505778cf1bd65854
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-09-17 13:27:36 +0900

    feat: ChatPage 에 useStomp 적용
    - useStomp 의 messages 를 messageGroupList 로 변환하는 util 추가
    
    - 테스트를 위해 aria-label 추가
    
    - input , button 추가

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,9 +1,15 @@
 "use client"
 
 import ChatPageTopbar from "@/app/chat/[chatId]/_components/ChatPageTopbar"
+import Button from "@/_common/_components/Button"
+import Input from "@/_common/_components/Input"
+import { MessageGroup } from "@/app/chat/[chatId]/_apis/getChatHistory"
 import MyMessageGroup from "@/app/chat/[chatId]/_components/MyMessageGroup"
 import OtherMessageGroup from "@/app/chat/[chatId]/_components/OtherMessageGroup"
 import { useFetchChatHistory } from "@/app/chat/[chatId]/_hooks/useFetchChatHistory"
+import useStomp from "@/app/chat/[chatId]/_hooks/useStomp"
+import { addMessageToGroup } from "@/app/chat/[chatId]/_utils/addMessageToGroup"
+import { useEffect, useState } from "react"
 
 interface ChatPageProps {
   params: {
@@ -12,12 +18,36 @@ interface ChatPageProps {
 }
 
 const ChatPage = ({ params }: ChatPageProps) => {
-  const { data } = useFetchChatHistory(params.chatId)
+  const [messageGroupList, setMessageGroupList] = useState<MessageGroup[]>([])
+  const { messages, sendMessage } = useStomp(params.chatId)
+  const { data, error } = useFetchChatHistory(params.chatId)
+
+  useEffect(() => {
+    if (data) {
+      setMessageGroupList(data.data.messageGroupList)
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setMessageGroupList((prev) =>
+        addMessageToGroup(messages[messages.length - 1], prev)
+      )
+    }
+  }, [messages])
+
+  if (error) {
+    throw new Error(error.message)
+  }
+
+  if (!data) {
+    return null
+  }
 
   return (
     <div>
       <ul aria-label="메세지 리스트">
-        {data.data.messageGroupList.map((messageGroup) => (
+        {messageGroupList.map((messageGroup) => (
           <li
             aria-label="메세지 그룹"
             key={messageGroup.id}>
@@ -37,6 +67,21 @@ const ChatPage = ({ params }: ChatPageProps) => {
           </li>
         ))}
       </ul>
+      <Input
+        aria-label="메세지 입력"
+        placeholder="메세지를 입력하세요"
+      />
+      <Button
+        aria-label="메세지 전송 버튼"
+        onClick={() => {
+          const input = document.querySelector("input")
+          if (input) {
+            sendMessage({ text: input.value })
+            input.value = ""
+          }
+        }}>
+        전송
+      </Button>
     </div>
   )
 }
