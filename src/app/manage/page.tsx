38859d27da1034fde8dd0262af5f7e192794e4f2
JangGwon <tlsakch510@naver.com>
2024-09-18 19:59:31 +0900

    feat : 개설한 모임 리스트 페이지 구현

---
+++ src/app/manage/page.tsx
@@ -0,0 +1,35 @@
+import MyManagingMeetingItem from "./_components/MyManagingMeetingItem"
+
+const mockData = [
+  {
+    title: "축구할사람 구해요",
+    date: "오늘",
+    startTime: "09:00",
+    endTime: "11:00",
+    totalPeople: 20,
+    nowPeople: 16,
+    meetingId: 1
+  },
+  {
+    title: "야구할사람 구해요",
+    date: "내일",
+    startTime: "19:00",
+    endTime: "20:00",
+    totalPeople: 20,
+    nowPeople: 16,
+    meetingId: 1
+  }
+]
+
+const Manage = () => {
+  return (
+    <div className="w-screen h-screen">
+      <h1 className="text-xl px-2 py-1">관리중인 모임</h1>
+      {mockData.map((item) => (
+        <MyManagingMeetingItem {...item} />
+      ))}
+    </div>
+  )
+}
+
+export default Manage
