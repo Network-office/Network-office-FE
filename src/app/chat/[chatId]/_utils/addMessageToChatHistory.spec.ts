80a04d8e01273ab2a7cb15a81719a826220e0b37
ienrum <azang0506@gmail.com>
2024-09-20 17:24:48 +0900

    refactor: messageGroupList -> chatHistory 로 이름 변경

--- src/app/chat/[chatId]/_utils/addMessageToChatHistory.spec.ts
+++ src/app/chat/[chatId]/_utils/addMessageToChatHistory.spec.ts
@@ -1,5 +1,5 @@
 import { MessageGroup } from "@/app/chat/[chatId]/_apis/getChatHistory"
-import { addMessageToGroup } from "@/app/chat/[chatId]/_utils/addMessageToGroup"
+import { addMessageToChatHistory } from "@/app/chat/[chatId]/_utils/addMessageToChatHistory"
 import { chatHistoryDataMap } from "@/mock/mockData/chatHistoryData"
 import { generateMyMessage } from "@/mock/mockData/chatMessageData"
 import { describe, expect, test } from "@jest/globals"
@@ -8,15 +8,15 @@ describe("addMessageToGroup", () => {
   test("마지막 메시지 그룹과 수신된 메세지의 id가 같을 때 마지막 메시지 그룹에 메세지를 추가한다", () => {
     const myId = "myId"
 
-    const messageGroup = chatHistoryDataMap["userMe"]
-    messageGroup[messageGroup.length - 1].userInfo.id = myId
+    const chatHistory = chatHistoryDataMap["userMe"]
+    chatHistory[chatHistory.length - 1].userInfo.id = myId
 
     const myMessage = generateMyMessage("new message")
     myMessage.userInfo.id = myId
 
-    const resultMessageGroup = addMessageToGroup(myMessage, messageGroup)
+    const modifiedChatHistory = addMessageToChatHistory(myMessage, chatHistory)
     expect(
-      resultMessageGroup[resultMessageGroup.length - 1].messages
+      modifiedChatHistory[modifiedChatHistory.length - 1].messages
     ).toContainEqual({
       id: myMessage.id,
       text: myMessage.message.text,
@@ -27,17 +27,17 @@ describe("addMessageToGroup", () => {
   test("마지막 메시지 그룹과 수신된 메세지의 id가 다를 때 새로운 메시지 그룹을 추가한다", () => {
     const myId = "myId"
 
-    const messageGroup = chatHistoryDataMap["adminMe"]
-    messageGroup[messageGroup.length - 1].userInfo.id = myId
+    const chatHistory = chatHistoryDataMap["adminMe"]
+    chatHistory[chatHistory.length - 1].userInfo.id = myId
 
     const myMessage = generateMyMessage("new message")
     myMessage.userInfo.id = "anotherId"
 
-    const resultMessageGroup = addMessageToGroup(myMessage, messageGroup)
-    expect(resultMessageGroup).toHaveLength(messageGroup.length + 1)
+    const modifiedChatHistory = addMessageToChatHistory(myMessage, chatHistory)
+    expect(modifiedChatHistory).toHaveLength(chatHistory.length + 1)
 
     expect(
-      resultMessageGroup[resultMessageGroup.length - 1].messages
+      modifiedChatHistory[modifiedChatHistory.length - 1].messages
     ).toContainEqual({
       id: myMessage.id,
       text: myMessage.message.text,
@@ -46,12 +46,12 @@ describe("addMessageToGroup", () => {
   })
 
   test("마지막 메시지 그룹이 없을 때 새로운 메시지 그룹을 추가한다", () => {
-    const messageGroup = [] as MessageGroup[]
+    const chatHistory = [] as MessageGroup[]
 
     const myMessage = generateMyMessage("new message")
     myMessage.userInfo.id = "anotherId"
 
-    const resultMessageGroup = addMessageToGroup(myMessage, messageGroup)
-    expect(resultMessageGroup).toHaveLength(1)
+    const modifiedChatHistory = addMessageToChatHistory(myMessage, chatHistory)
+    expect(modifiedChatHistory).toHaveLength(1)
   })
 })
