ab58a8b31e7998eea8fe455d74493a672ee0cc81
ienrum <azang0506@gmail.com>
2024-09-24 12:36:40 +0900

    feat: Input 컴포넌트와 쓰이는 예제 추가

--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -1,7 +1,8 @@
 import Button from "@/_common/_components/Button"
+import Input from "@/_common/_components/Input"
 import Tooltip from "@/_common/_components/Tooltip"
 import { Meta, StoryObj } from "@storybook/react"
-import { fn } from "@storybook/test"
+import { FormEvent, useState } from "react"
 
 const meta = {
   title: "Common/Tooltip",
@@ -37,8 +38,45 @@ export const AutoOpen: Story = {
   args: {
     content: "Tooltip content",
     children: <Button>Trigger</Button>,
-
     side: "top",
-    autoOpen: true
+    defaultOpen: true
+  }
+}
+
+export const WithInput: Story = {
+  render: (args) => {
+    const [open, setOpen] = useState(false)
+    const [content, setContent] = useState("")
+
+    const handleSubmit = (e: FormEvent) => {
+      e.preventDefault()
+      if (content === "") {
+        setOpen(true)
+      } else {
+        setOpen(false)
+      }
+    }
+
+    return (
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Tooltip
+          {...args}
+          content="input is required"
+          side="top"
+          open={open}>
+          <Input
+            label="Input"
+            iconPosition="right"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Tooltip>
+      </form>
+    )
+  },
+  args: {
+    content: "Tooltip content",
+    children: <Button>Trigger</Button>,
+    side: "top"
   }
 }
