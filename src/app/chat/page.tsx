77a413ef1054a2450b645ccd68b9168720100b6f
ienrum <azang0506@gmail.com>
2024-09-25 17:27:27 +0900

    refactor: Page 서 ChatRoomList 컴포넌트로 분리

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,27 +1,14 @@
-"use client"
+import ChatRoomList from "@/app/chat/_components/ChatRoomList"
 
-import ChatRoomItem from "@/app/chat/_components/ChatRoomItem"
-import useGetChatRoomList from "@/app/chat/_hooks/queries/useGetChatRoomList"
-import { useSearchParams } from "next/navigation"
+interface ChatRoomPageProps {
+  searchParams: { role?: string }
+}
 
-const ChatRoomPage = () => {
-  const searchParams = useSearchParams()
+const ChatRoomPage = ({ searchParams }: ChatRoomPageProps) => {
+  const role = searchParams.role as "admin" | "user" | undefined //TODO: "admin" | "user" 를 enum 으로 정의할까?
+  //TODO: prefetch 로직 추가
 
-  const role = searchParams.get("role") as "admin" | "user" | undefined //TODO: "admin" | "user" 를 enum 으로 정의할까?
-  //TODO: prefetch
-
-  const { data: rooms } = useGetChatRoomList(role)
-
-  return (
-    <div className="flex flex-col">
-      {rooms?.map((room) => (
-        <ChatRoomItem
-          key={room.title}
-          room={room}
-        />
-      ))}
-    </div>
-  )
+  return <ChatRoomList role={role} />
 }
 
 export default ChatRoomPage
