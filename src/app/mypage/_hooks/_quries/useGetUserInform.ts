1e45fafbb5b5b7a5b5940a65a6336a0f6c83c8dc
JangGwon <tlsakch510@naver.com>
2024-09-28 23:06:25 +0900

    feat :  useGetUserInform 훅 useQuery -> useSuspenseQuery 훅으로 변경

--- src/app/mypage/_hooks/_quries/useGetUserInform.ts
+++ src/app/mypage/_hooks/_quries/useGetUserInform.ts
@@ -1,10 +1,10 @@
 "use client"
 
-import { useQuery } from "@tanstack/react-query"
+import { useSuspenseQuery } from "@tanstack/react-query"
 import getUserInform from "../../_api/getUserInform"
 
 const useGetUserInform = (userId: string) => {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading } = useSuspenseQuery({
     queryKey: ["userInform", userId],
     queryFn: () => getUserInform(userId)
   })
