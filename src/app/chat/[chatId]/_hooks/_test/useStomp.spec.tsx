013965c0802c72cc43799181770a6228a7508512
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-09-28 22:24:15 +0900

    feat: GET chatHistory api 패칭 추가 (#56)
    * refactor: 컴포넌트명의 Messages 를 MessageGroup 으로 변경 및 props type 컴포넌트 내부로 이동
    
    * refactor: 단일 Message 관련 컴포넌트들을 폴더로 묶기
    
    * feat: chatApi 구현
    
    * refactor: GetChatHistoryResponse 타입을 getChatHistory 파일로 이동
    
    * feat: 이전 채팅 히스토리를 가져오는 fetch 구현
    
    - useStomp 에 socket 실행시 chat 히스토리 캐시 무효화
    
    * feat: 이전 history 를 가져오는 page 컴포넌트 작성
    
    * refactor: badge import 경로 수정
    
    * fix: jest fetch 에러 수정
    
    * feat: page test 추가
    
    * feat: page test 추가
    
    * feat: chat history response 필드에 title 추가
    
    * feat: chatPage topbar 구현
    
    * feat: useStomp test 에 queryClient provider 추가

--- src/app/chat/[chatId]/_hooks/_test/useStomp.spec.tsx
+++ src/app/chat/[chatId]/_hooks/_test/useStomp.spec.tsx
@@ -4,17 +4,33 @@ import useStomp from "@/app/chat/[chatId]/_hooks/useStomp"
 import { describe, expect, test } from "@jest/globals"
 import { act, renderHook, waitFor } from "@testing-library/react"
 import { Client } from "@stomp/stompjs"
+
 import {
   mockActivate,
   mockPublish,
   subscribeCallback
 } from "../../../../../../__mocks__/@stomp/stompjs"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
 
 jest.mock("@stomp/stompjs")
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      staleTime: 0
+    }
+  }
+})
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+
 describe("useStomp", () => {
   test("연결이 되어야 한다", () => {
-    renderHook(() => useStomp("1"))
+    renderHook(() => useStomp("1"), { wrapper })
 
     // stompjs.Client 생성자가 호출되었는지 확인
     expect(Client).toHaveBeenCalled()
@@ -23,7 +39,7 @@ describe("useStomp", () => {
   })
 
   test("메시지를 보낼 수 있다", () => {
-    const { result } = renderHook(() => useStomp("1"))
+    const { result } = renderHook(() => useStomp("1"), { wrapper })
 
     act(() => {
       result.current.sendMessage({ id: "1", text: "test" })
@@ -37,7 +53,7 @@ describe("useStomp", () => {
   })
 
   test("메시지를 받으면 messages 에 추가된다", async () => {
-    const { result } = renderHook(() => useStomp("1"))
+    const { result } = renderHook(() => useStomp("1"), { wrapper })
 
     // subscribeCallback 함수를 호출하여 메시지를 전달
     act(() => {
