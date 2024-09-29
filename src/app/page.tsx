cd18642029be83535298351e40e79b2b623489a2
JangGwon <tlsakch510@naver.com>
2024-09-30 01:24:12 +0900

    test : useToast 테스트용 코드제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-"use client"
-
-import { useToast } from "@/_common/_hooks/useToast"
-import { Button } from "@/components/ui/button"
-
-export default function ToastDemo() {
-  const { toast } = useToast()
-
-  return (
-    <Button
-      variant="outline"
-      onClick={() => {
-        toast({
-          content: <p className="mx-auto">ㅅㅂ</p>
-        })
-      }}>
-      Add to calendar
-    </Button>
-  )
+export default function Home() {
+  return <></>
 }
