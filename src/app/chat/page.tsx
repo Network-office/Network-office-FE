6dbce1cef9c20834eab10a552cdc0a16a0bf12d5
ienrum <azang0506@gmail.com>
2024-10-03 12:57:20 +0900

    feat: ChatRoomPage 에 Topbar 추가

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,3 +1,4 @@
+import Topbar from "@/_common/_components/Topbar"
 import ChatRoomListWithTabs from "@/app/chat/_components/ChatRoomListWithTabs"
 
 interface ChatRoomPageProps {
@@ -8,7 +9,15 @@ const ChatRoomPage = ({ searchParams }: ChatRoomPageProps) => {
   const role = searchParams.role as "admin" | "user" | undefined //TODO: "admin" | "user" 를 enum 으로 정의할까?
   //TODO: prefetch 로직 추가
 
-  return <ChatRoomListWithTabs defaultRole={role} />
+  return (
+    <div className="relative">
+      <Topbar
+        title="채팅방"
+        rightContent={<Topbar.AlarmLink />}
+      />
+      <ChatRoomListWithTabs defaultRole={role} />
+    </div>
+  )
 }
 
 export default ChatRoomPage
