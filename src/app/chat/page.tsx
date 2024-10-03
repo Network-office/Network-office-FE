e80918765af6c417128f1a45518d92a320c7f16a
ienrum <azang0506@gmail.com>
2024-10-03 21:40:00 +0900

    feat: ChatRoomPage 에서 ssr prefetch 추가

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,22 +1,32 @@
 import Topbar from "@/_common/_components/Topbar"
+import { getQueryClient } from "@/_common/_utils/getClientQuery"
+import getChatRoomList from "@/app/chat/_apis/getChatRoomList"
 import ChatRoomListWithTabs from "@/app/chat/_components/ChatRoomListWithTabs"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 
 interface ChatRoomPageProps {
-  searchParams: { role?: string }
+  searchParams: { role?: "admin" | "user" }
 }
 
 const ChatRoomPage = ({ searchParams }: ChatRoomPageProps) => {
-  const role = searchParams.role as "admin" | "user" | undefined //TODO: "admin" | "user" 를 enum 으로 정의할까?
-  //TODO: prefetch 로직 추가
+  const role = searchParams.role === undefined ? "all" : searchParams.role
+  const queryClient = getQueryClient()
+
+  void queryClient.prefetchQuery({
+    queryKey: ["chatRoomList", role],
+    queryFn: () => getChatRoomList(role)
+  })
 
   return (
-    <div className="relative">
-      <Topbar
-        title="채팅방"
-        rightContent={<Topbar.AlarmLink />}
-      />
-      <ChatRoomListWithTabs defaultRole={role} />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="relative">
+        <Topbar
+          title="채팅방"
+          rightContent={<Topbar.AlarmLink />}
+        />
+        <ChatRoomListWithTabs defaultRole={role} />
+      </div>
+    </HydrationBoundary>
   )
 }
 
