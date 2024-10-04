68e92347e29e8e418d031a8e1695eaddcd2b5bc6
JangGwon <tlsakch510@naver.com>
2024-10-04 20:24:48 +0900

    feat : 댓글 달기 Ui 구현

--- src/app/feed/[feedId]/_components/FeedCommentInputBar.tsx
+++ src/app/feed/[feedId]/_components/FeedCommentInputBar.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import { Send } from "lucide-react"
+
 const FeedCommentInputBar = () => {
+  const onClickSendButton = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
   return (
-    <div className="fixed w-screen bg-slate-200 h-[40px] bottom-0 left-0">
-      <input className="w-[90%] border-2" />
-    </div>
+    <form className="fixed w-screen bg-slate-200 h-[50px] bottom-0 left-0 flex justify-center gap-2 items-center">
+      <input className="w-[85%] h-[80%] border-2 px-2 rounded-lg" />
+      <button
+        type="submit"
+        onSubmit={onClickSendButton}
+        className="flex items-center">
+        <Send />
+      </button>
+    </form>
   )
 }
 
