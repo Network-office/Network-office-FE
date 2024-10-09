5a004ac40e7b9157248439923cd68013c1e3f4ba
JangGwon <tlsakch510@naver.com>
2024-10-09 19:32:19 +0900

    feat : 검색 기능 및 검색 결과 ui 구현

--- src/app/meeting/_components/SearchBar.tsx
+++ src/app/meeting/_components/SearchBar.tsx
@@ -1,14 +1,52 @@
+"use client"
+import { useState } from "react"
 import { Search, AlignJustifyIcon } from "lucide-react"
+import PlaceTypes from "@/_common/_utils/searchPlace/type"
+import searchPlace from "@/_common/_utils/searchPlace"
 
 const SearchBar = () => {
+  const [inputKeyword, setInputKeyWord] = useState("")
+  const [searchResult, setSearchResult] = useState<PlaceTypes[] | null>(null)
+
+  const searchBarhandle = async () => {
+    if (inputKeyword.length > 1) {
+      const result = await searchPlace(inputKeyword)
+      setSearchResult(result)
+    } else {
+      setSearchResult(null)
+    }
+  }
+  const onClickSearchResult = (selectedResult: PlaceTypes) => {
+    setSearchResult(null)
+  }
+
   return (
-    <div className="w-[85%] h-[40px] mt-3 mx-auto rounded-lg  border-black  bg-white drop-shadow-xl flex gap-2 justify-center items-center">
-      <AlignJustifyIcon size={24} />
-      <input className="w-[70%] border-[1px] rounded-sm px-2" />
-      <Search
-        size={24}
-        className="py-auto h-fit"
-      />
+    <div>
+      <div className="w-[85%] h-[40px] mt-3 mx-auto rounded-lg  border-black  bg-white drop-shadow-lg flex gap-2 justify-center items-center">
+        <AlignJustifyIcon size={24} />
+        <input
+          onChange={(event) => setInputKeyWord(event.target.value)}
+          onKeyUp={searchBarhandle}
+          className="w-[70%] border-[1px] rounded-sm px-2"
+        />
+        <Search
+          size={24}
+          className="py-auto h-fit"
+        />
+      </div>
+      <ul className="w-[85%] h-fit mt-1 mx-auto rounded-sm shadow-2xl">
+        {searchResult &&
+          searchResult?.length > 0 &&
+          searchResult.map((item) => (
+            <li
+              className="w-full px-3 py-2 text-lg bg-white border-[1px]"
+              key={`%${item.x}${item.y}${item.distance}`}>
+              <button onClick={() => onClickSearchResult(item)}>
+                {item.jibunAddress}
+              </button>
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
