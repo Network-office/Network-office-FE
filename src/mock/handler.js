c61eb349a3ef66a384565267bee9616f18960208
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-10-09 15:36:08 +0900

    Revert "feat: 채팅방 목록 구현 (#64)"
    This reverts commit a841403dd9f3d57eb2fc7a1ee5e923c1fc9dcff8.

--- src/mock/handler.js
+++ src/mock/handler.js
@@ -1,4 +1,3 @@
 import meetingApi from "./api/meetingApi"
-import chatRoomApi from "./api/chatRoomApi"
 
-export const handlers = [...meetingApi, ...chatRoomApi]
+export const handlers = [...meetingApi]
