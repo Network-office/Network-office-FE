b0171630392679a429e44a51c8b35c5401633d3e
Kim0426 <706shin1728@naver.com>
2024-10-09 20:55:28 +0900

    chore: rewrite 프록시 설정 및 https dev run 스크립트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  rewrites: async () => [
+    {
+      source: "/dev/:path*",
+      destination: "https://network-office.duckdns.org/dev/:path*"
+    }
+  ]
 }
 
 export default nextConfig
