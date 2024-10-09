5ae838d067f06b24d0cf6d2de34a75ecc12e194f
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-10-09 15:58:36 +0900

    feat: 채팅방 목록 구현 (#86)
    * feat: chatRoom mock 데이터 추가
    
    * feat: chatRoom msw 핸들러 추가
    
    * feat: chatRoom 페이지 fetch 함수 추가
    
    * feat: role 파라미터 추가
    
    * feat: 데이터 생성후 export
    
    * refactor: 채팅방 필터링 추가
    
    * feat: chatRoomPage 추가
    
    * refactor: Page 서 ChatRoomList 컴포넌트로 분리
    
    * feat: ChatRoomList 에 Tabs 컴포넌트 래핑 (#73)
    
    * chore: shadcn ui tabs 컴포넌트 추가
    
    * feat: ChatRoomList 에 Tabs 컴포넌트 래핑
    
    * feat: chatRoomItem Link 태그 추가
    
    * refactor: tab 순서변경
    
    * feat: id 추가
    
    * refactor: pathParams undefined  -> "all"
    
    * refactor: queryParam role 과 tab 에 사용되는 role 분리
    
    * refactor: role 에서 defaultRole 로 prop 명 수정
    
    * refactor: storybook cors 해결을 위해 6006 포트 추가
    
    * feat: ChatRoomListWithTabs 컴포넌트 스토리 추가

---
+++ src/app/chat/_apis/getChatRoomList.ts
@@ -0,0 +1,34 @@
+import { http } from "@/lib/http"
+
+export interface ChatRoom {
+  id: string
+  title: string
+  authorInfo: {
+    name: string
+    avatarSrc: string
+  }
+  currentMessage: {
+    content: string
+    timestamp: string
+  }
+  myRole: "admin" | "user"
+  unreadCount?: number
+}
+
+export interface ChatRoomListResponse {
+  rooms: ChatRoom[]
+}
+
+const getChatRoomList = async (role: "admin" | "user" | "all") => {
+  const response = await http<ChatRoomListResponse>(
+    `http://localhost:8080/api/chat-room/${role}`,
+    {
+      cache: "no-store",
+      method: "GET"
+    }
+  )
+
+  return response
+}
+
+export default getChatRoomList
