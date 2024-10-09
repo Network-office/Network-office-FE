a1a5db289739a449805f0aa24a72e1e19c6c5600
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-10-09 15:58:36 +0900

    feat: 채팅방 목록 구현 (#86)
    * feat: chatRoom mock 데이터 추가
    
    * feat: chatRoom msw 핸들러 추가
    
    * feat: chatRoom 페이지 fetch 함수 추가
    
    * feat: role 파라미터 추가
    
    * feat: 데이터 생성후 export
    
    * refactor: 채팅방 필터링 추가
    
    * feat: chatRoomPage 추가
    
    * refactor: Page 서 ChatRoomList 컴포넌트로 분리
    
    * feat: ChatRoomList 에 Tabs 컴포넌트 래핑 (#73)
    
    * chore: shadcn ui tabs 컴포넌트 추가
    
    * feat: ChatRoomList 에 Tabs 컴포넌트 래핑
    
    * feat: chatRoomItem Link 태그 추가
    
    * refactor: tab 순서변경
    
    * feat: id 추가
    
    * refactor: pathParams undefined  -> "all"
    
    * refactor: queryParam role 과 tab 에 사용되는 role 분리
    
    * refactor: role 에서 defaultRole 로 prop 명 수정
    
    * refactor: storybook cors 해결을 위해 6006 포트 추가
    
    * feat: ChatRoomListWithTabs 컴포넌트 스토리 추가

---
+++ src/mock/mockData/chatRoomData.ts
@@ -0,0 +1,30 @@
+import {
+  ChatRoom,
+  ChatRoomListResponse
+} from "@/app/chat/_apis/getChatRoomList"
+import { faker } from "@faker-js/faker"
+
+const generateChatRoomData = (): ChatRoom => {
+  return {
+    id: faker.string.uuid(),
+    title: faker.lorem.sentence(),
+    authorInfo: {
+      name: faker.internet.userName(),
+      avatarSrc: faker.image.avatar()
+    },
+    currentMessage: {
+      content: faker.lorem.sentence(),
+      timestamp: faker.date.recent().toISOString()
+    },
+    myRole: faker.helpers.arrayElement(["admin", "user"]),
+    unreadCount: faker.number.int(100)
+  }
+}
+
+const generateChatRoomListData = (length: number): ChatRoomListResponse => {
+  return {
+    rooms: Array.from({ length }, generateChatRoomData)
+  }
+}
+
+export const chatRoomListData = generateChatRoomListData(10)
