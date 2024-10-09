cfc2b3dad6c0b267547e8235930dafe949c3b569
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-10-09 15:01:09 +0900

    feat: Tooltip 컴포넌트 구현 (#62)
    * chore: shadcn Tooltip 컴포넌트 추가
    
    * feat: Tooltip 컴포넌트 구현
    
    * feat: Tooltip 스토리 구현
    
    * feat: click 으로 open 기능 추가
    
    * feat: story 추가
    
    * refactor: "Hover me" -> Trigger button
    
    * refactor: autoOpen -> defaultOpen 및 click 이벤트 제거
    
    * refactor: types 폴더로 props 타입 분리
    
    * feat: Input 컴포넌트와 쓰이는 예제 추가
    
    * feat: 일정 시간후 닫히는 기능 추가
    
    * refactor: durationOpenState -> durationOpen
    
    * refactor: useDurationOpen 훅을 공용 훅으로 분리

---
+++ src/stories/Tooltip.stories.tsx
@@ -0,0 +1,83 @@
+import Button from "@/_common/_components/Button"
+import Input from "@/_common/_components/Input"
+import Tooltip from "@/_common/_components/Tooltip"
+import { Meta, StoryObj } from "@storybook/react"
+import { FormEvent, useState } from "react"
+
+const meta = {
+  title: "Common/Tooltip",
+  component: Tooltip,
+  parameters: {
+    layout: "centered"
+  },
+  tags: ["autodocs"]
+} satisfies Meta<typeof Tooltip>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    content: "Tooltip content",
+    children: <Button>Trigger</Button>,
+    side: "top"
+  }
+}
+
+export const WithArrow: Story = {
+  args: {
+    content: "Tooltip content",
+    children: <Button>Trigger</Button>,
+    side: "top",
+    arrow: true
+  }
+}
+
+export const AutoOpen: Story = {
+  args: {
+    content: "Tooltip content",
+    children: "Auto open",
+    side: "top",
+    defaultOpen: true,
+    openDuration: 1000
+  }
+}
+
+export const WithInput: Story = {
+  render: (args) => {
+    const [open, setOpen] = useState(false)
+    const [content, setContent] = useState("")
+
+    const handleSubmit = (e: FormEvent) => {
+      e.preventDefault()
+      if (content === "") {
+        setOpen(true)
+      } else {
+        setOpen(false)
+      }
+    }
+
+    return (
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Tooltip
+          {...args}
+          content="input is required"
+          side="top"
+          open={open}>
+          <Input
+            label="Input"
+            iconPosition="right"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </Tooltip>
+      </form>
+    )
+  },
+  args: {
+    content: "Tooltip content",
+    children: <Button>Trigger</Button>,
+    side: "top"
+  }
+}
