e3a6e46b434c5272822dbd6c18703c348c3bb817
JangGwon <tlsakch510@naver.com>
2024-10-09 16:08:50 +0900

    test : 테스트 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,5 @@
 "use client"
-import Chips from "@/_common/_components/Chips"
-import Chip from "@/_common/_components/Chips/chip"
 
 export default function Home() {
-  const handleSelectionChange = (selectedIds: string[]) => {
-    console.log("Selected chips:", selectedIds)
-  }
-
-  return (
-    <div>
-      <Chips
-        selectionMode="multiple"
-        maxSelection={3}
-        onSelectionChange={handleSelectionChange}>
-        <Chip
-          label="칩 1"
-          className="custom-chip"
-        />
-        <Chip
-          label="칩 2"
-          color="primary"
-        />
-        <Chip
-          label="칩 3"
-          color="secondary"
-        />
-        <Chip label="칩 4" />
-      </Chips>
-    </div>
-  )
+  return <div></div>
 }
