64ec98baf1b004f40c3cafc6941cd551dba2e43e
JangGwon <tlsakch510@naver.com>
2024-10-11 16:27:58 +0900

    style : 모임 관리 설정 모달 버튼 디자인 변경

--- src/app/feed/_components/FeedFootBar.tsx
+++ src/app/feed/_components/FeedFootBar.tsx
@@ -3,11 +3,11 @@ import { Plus } from "lucide-react"
 
 const FeedFootBar = () => {
   return (
-    <div className="fixed w-full bottom-6 left-0 z-10 px-4 flex justify-end ">
+    <div className="fixed w-full bottom-20 left- z-10 px-4 flex justify-end ">
       <Link href="feed/create">
         <Plus
-          className="w-[50px] h-[50px] bg-slate-200 rounded-full px-1 shadow-xl"
-          size={44}
+          className="bg-slate-200 rounded-full px-1 mr-1 shadow-2xl"
+          size={48}
         />
       </Link>
     </div>
