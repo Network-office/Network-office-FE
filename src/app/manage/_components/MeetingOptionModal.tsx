d71eebff0e3e041832451e56f16ea4315a047fb7
JangGwon <tlsakch510@naver.com>
2024-10-11 16:27:58 +0900

    style : 모임 관리 설정 모달 버튼 디자인 변경

--- src/app/manage/_components/MeetingOptionModal.tsx
+++ src/app/manage/_components/MeetingOptionModal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import useCloseMeeting from "../_hooks/_mutations/useCloseMeeting"
 import useCancelMeeting from "../_hooks/_mutations/useCancelMeeting"
 import CancelReasonModal from "./CancleReasonModal"
+import Button from "@/_common/_components/Button"
 
 interface MeetingOptionModalProps {
   meetingId: number
@@ -49,19 +50,19 @@ const MeetingOptionModal = ({
       <div className="w-[90%] h-[280px] z-50 bg-white left-1/2 top-[30%] -translate-x-1/2 -translate-y-[30%] absolute rounded-sm  shadow-xl">
         <h1 className="text-xl px-4 py-4 font-medium ">모임 관리</h1>
         <div className="flex flex-col gap-4">
-          <button className="w-[80%] h-[52px] bg-blue-200 rounded-md shadow-xl mx-auto font-semibold text-xl text-white">
+          <Button className="w-[80%] h-[52px] rounded-md shadow-xl mx-auto font-semibold text-xl text-white">
             게시글 수정하기
-          </button>
-          <button
+          </Button>
+          <Button
             onClick={handleCloseMeeting}
-            className="w-[80%] h-[52px] bg-blue-200 rounded-md shadow-xl mx-auto font-semibold text-xl text-white">
+            className="w-[80%] h-[52px] rounded-md shadow-xl mx-auto font-semibold text-xl text-white">
             모집 마감하기
-          </button>
-          <button
+          </Button>
+          <Button
             onClick={() => setShowCancelReason(true)}
-            className="w-[80%] h-[52px] bg-blue-200 rounded-md shadow-xl mx-auto font-semibold text-xl text-white">
+            className="w-[80%] h-[52px] rounded-md shadow-xl mx-auto font-semibold text-xl text-white">
             모임 파토하기
-          </button>
+          </Button>
         </div>
       </div>
       {showCancelReason && (
