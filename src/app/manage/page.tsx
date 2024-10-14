b965766c48f58154987618abf70e10f162be2fc9
JangGwon <tlsakch510@naver.com>
2024-10-14 18:02:47 +0900

    feat : 내가 참여 중인 모임 아코디언은 기본적으로 열려있는 기능 구현

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -6,8 +6,10 @@ import Participate from "./_components/ParticipatingMeetings"
 const Manage = () => {
   return (
     <div className="w-screen h-fit px-4 mb-12">
-      <Accordion type="multiple">
-        <Accordion.Item value="participate" >
+      <Accordion
+        type="multiple"
+        defaultValue={["participate"]}>
+        <Accordion.Item value="participate">
           <Accordion.Trigger className="w-full">
             <p className="text-lg font-semibold">내가 참여 중인 모임</p>
           </Accordion.Trigger>
