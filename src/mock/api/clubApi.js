e452abd51df42c6f1c5d8d9a995e9807c3669eac
JangGwon <tlsakch510@naver.com>
2024-10-17 17:12:23 +0900

    feat : 새로운 동호회 조회 Mock api 추가

---
+++ src/mock/api/clubApi.js
@@ -0,0 +1,15 @@
+import { http, HttpResponse } from "msw"
+import clubData from "../mockData/clubData"
+
+const handlers = [
+  http.get(`http://localhost:8080/api/club/new`, () => {
+    return new HttpResponse(
+      JSON.stringify({
+        clubList: clubData.slice(0, 4)
+      }),
+      { status: 200, headers: { "Content-Type": "application/json" } }
+    )
+  })
+]
+
+export default handlers
---
+++ src/mock/mockData/clubData.ts
@@ -0,0 +1,32 @@
+const clubData = [
+  {
+    clubId: "1231",
+    name: "문화 문화 문화",
+    imageUrl: null,
+    meetingFrequency: "매주 수요일",
+    genre: "문화"
+  },
+  {
+    clubId: "12312",
+    name: "스포츠 스포츠",
+    imageUrl: null,
+    meetingFrequency: "매주 화요일",
+    genre: "스포츠"
+  },
+  {
+    clubId: "123412",
+    name: "스포츠 스포츠",
+    imageUrl: null,
+    meetingFrequency: "매주 화요일",
+    genre: "스포츠"
+  },
+  {
+    clubId: "1142312",
+    name: "스포츠 스포츠",
+    imageUrl: null,
+    meetingFrequency: "매주 화요일",
+    genre: "스포츠"
+  }
+]
+
+export default clubData
