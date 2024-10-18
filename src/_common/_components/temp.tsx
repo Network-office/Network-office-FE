76cc04d4393e4c7b94e81816f5dd4fa25cc6125f
JangGwon <tlsakch510@naver.com>
2024-10-19 03:02:07 +0900

    test : 테스트용 코드 제거

--- src/_common/_components/temp.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react"
-import CustomError from "@/lib/CustomError"
-
-export default function Temp() {
-  useEffect(() => {
-    throw new CustomError("페이지를 발견할 수 없습니다", 400)
-  }, [])
-  return <div></div>
-}
