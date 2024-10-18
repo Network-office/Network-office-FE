c82142947c90b52ae4af89a36f05de7bff2919aa
JangGwon <tlsakch510@naver.com>
2024-10-18 20:35:12 +0900

    feat : CarouselNavbar 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 "use client"
 
-import ErrorBoundary from "@/_common/_components/ErrorBoundary"
-import Temp from "@/_common/_components/temp"
+import Carousel from "@/_common/_components/Carousel"
+
 export default function Home() {
   return (
     <div>
-      <ErrorBoundary>
-        <Temp />
-        <p>안녕</p>
-      </ErrorBoundary>
+      <Carousel
+        autoPlay
+        className="w-full max-w-xs">
+        <Carousel.Slide>
+          <p>Slide 1</p>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <p>Slide 2</p>
+        </Carousel.Slide>
+        <Carousel.Slide>
+          <p>Slide 3</p>
+        </Carousel.Slide>
+      </Carousel>
     </div>
   )
 }
