088bb2809dec59d894735ef431d906f0e26aecb0
kimhyojung <706shin1728@naver.com>
2024-10-20 16:43:48 +0900

    chore: change jest polyfill

--- src/app/api/phone/useGeneratePhoneVerfification.tsx
+++ src/app/api/phone/useGeneratePhoneVerfification.tsx
@@ -1,26 +1,42 @@
 import { useMutation } from "@tanstack/react-query"
+import { useGetCSRFToken } from "../auth/csrf"
 
 const generatePhoneVerificationCode = async ({
   phoneNumber
 }: {
   phoneNumber: string
-}) => {
+}): Promise<{ code: string }> => {
   try {
-    const response = await fetch("/server/api/v1/verification/phone/code", {
+    const xsrfToken = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("XSRF-TOKEN="))
+      ?.split("=")[1]
+    const response = await fetch("/dev/api/v1/verification/phone/code", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ phoneNumber })
+      credentials: "include",
+      body: JSON.stringify({ phoneNumber: phoneNumber.replaceAll("-", "") })
     })
-    return response
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok")
+    }
+
+    const data = await response.json()
+    return data
   } catch (e) {
     throw e
   }
 }
 
 export const useGeneratePhoneVerification = () => {
+  const newCsrfToken = useGetCSRFToken()
   return useMutation({
-    mutationFn: generatePhoneVerificationCode
+    mutationFn: generatePhoneVerificationCode,
+    onError: () => {
+      newCsrfToken.mutate()
+    }
   })
 }
