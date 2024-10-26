80df144e336e542533100228fd30917dc8392bfa
JangGwon <40955023+JuJangGwon@users.noreply.github.com>
2024-10-27 01:28:58 +0900

    Revert "setting : E2E 테스트 자동화 (#115)"
    This reverts commit 7573e61183a307f865c2958de38289fb57ab8393.

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
 
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {},
-    env: {
-      defaultCommandTimeout: 10000,
-      pageLoadTimeout: 10000
-    }
+    setupNodeEvents(on, config) {}
   }
 })
