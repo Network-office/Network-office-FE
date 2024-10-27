89f76fbe87aeb97ad54b24aa6f03a1d2f7124c24
JangGwon <tlsakch510@naver.com>
2024-10-27 16:41:49 +0900

    design : 마커 디자인 변경

--- src/_common/_hooks/useNaverMap/index.tsx
+++ src/_common/_hooks/useNaverMap/index.tsx
@@ -52,11 +52,25 @@ const useNaverMap = (
     }
 
     makers?.forEach((newMaker) => {
+      const markerColor = "#17181B"
+      const markerContent = `
+        <svg width="30" height="45" viewBox="0 0 30 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15 0C6.71573 0 0 6.71573 0 15C0 26.25 15 45 15 45C15 45 30 26.25 30 15C30 6.71573 23.2843 0 15 0Z" fill="${markerColor}"/>
+          <circle cx="15" cy="15" r="12" fill="white"/>
+        </svg>
+      `
+
       const maker = new naver.maps.Marker({
         position: new naver.maps.LatLng(newMaker.lat, newMaker.lng),
         clickable: true,
-        map: mapRef.current!
+        map: mapRef.current!,
+        icon: {
+          content: markerContent,
+          size: new naver.maps.Size(30, 45),
+          anchor: new naver.maps.Point(15, 45)
+        }
       })
+
       maker.addListener("click", () => {
         makerOption?.markerClickHandler(newMaker)
       })
