fe528f15664574a9384807e68651ae0a8ad519a8
kimhyojung <706shin1728@naver.com>
2024-11-07 23:35:13 +0900

    feat: config변경

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
   },
   images: {
     domains: ["placekitten.com", "picsum.photos", "source.unsplash.com"]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: `${process.env.NEXT_PUBLIC_APL_URL}/dev/api/v1/:path*`
+      }
+    ]
   }
 }
 
